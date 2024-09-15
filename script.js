a2e633159e9f588f8c4fde9cc50956a8f969d43b
krishna016agarwal <krishna016agrawal@gmail.com>
2024-09-15 09:04:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 a=document.querySelector("#ooo");
 b=document.querySelector("body");
 c=document.querySelector("#hh");
-a.addEventListener("click",()=>{
- window.open('index2.html','_blanck');
+a.addEventListener("click",()=>{  // click on get started button beside email address button
+ window.open('index2.html','_blanck'); // opens 2 page of netflix
     
     
   })
