9dfa140fad510e85e3c73548ecf6d1bfa5348e3d
krishna016agarwal <krishna016agrawal@gmail.com>
2024-09-15 08:54:39 +0530

    Add files via upload

---
+++ scc.js
@@ -0,0 +1,4 @@
+a=document.querySelector(".logo");
+a.addEventListener("click",()=>{
+    window.open('index.html','_blanck');
+  })
---
+++ script.js
@@ -0,0 +1,14 @@
+a=document.querySelector("#ooo");
+b=document.querySelector("body");
+c=document.querySelector("#hh");
+a.addEventListener("click",()=>{
+    if(c!==""){
+        window.open('index2.html','_blanck');
+    }else if(c===""){
+        alert("enter email")
+    }
+    
+  })
+
+
+
