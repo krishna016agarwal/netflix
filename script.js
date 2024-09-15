38f148b0821aaf63f3f9686692c7b45ed5a16a7f
krishna016agarwal <krishna016agrawal@gmail.com>
2024-09-15 09:01:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ a=document.querySelector("#ooo");
 b=document.querySelector("body");
 c=document.querySelector("#hh");
 a.addEventListener("click",()=>{
-    if(c!==""){
-        window.open('index2.html','_blanck');
-    }
+ window.open('index2.html','_blanck');
+    
     
   })
 
